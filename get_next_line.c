343db07e0a6410b7995d566f63d33a4b9e4d0410
derfl8 <alfred.begou@gmail.com>
2025-12-10 09:13:17 +0100

    add extract line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: abegou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 18:41:46 by abegou            #+#    #+#             */
-/*   Updated: 2025/12/07 15:42:25 by abegou           ###   ########.fr       */
+/*   Updated: 2025/12/10 09:12:55 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,24 @@
 
 char	*extract_line(char *stash)
 {
-	char	*line;
-	
-	line = ft_substr(stash, 0, ft_strlen(ft_strchr(stash, '\n')));
-	return (line);
+	char	*new_line;
+	size_t	len;
+
+	len = 0;
+	new_line = ft_strchr(stash, '\n');
+	if (new_line)
+	{
+		len = new_line - stash + 1;
+		new_line = ft_substr(stash, 0, len);
+	}
+	return (new_line);
 }
 
 char	*read_stash(int fd, char *stash)
 {
 	char	*buffer;
-	char	*line;
 	int		read_res;
-	
+
 	read_res = 1;
 	buffer = ft_calloc(sizeof(char), (size_t)BUFFER_SIZE + 1);
 	if (!buffer)
@@ -40,11 +46,8 @@ char	*read_stash(int fd, char *stash)
 		}
 		buffer[read_res] = '\0';
 		stash = ft_strjoin(stash, buffer);
-		if (ft_strchr(buffer, '\n'))
-		{
-			line = extract_line(stash);
+		if (ft_strchr(stash, '\n'))
 			break ;
-		}
 	}
 	free(buffer);
 	return (stash);
@@ -53,11 +56,14 @@ char	*read_stash(int fd, char *stash)
 char	*get_next_line(int fd)
 {
 	static char	*stash;
+	char		*line;
 
 	if (fd < 0 || fd > 1023 || BUFFER_SIZE <= 0)
 		return (NULL);
 	stash = read_stash(fd, stash);
 	if (!stash || stash[0] == 0)
 		return (NULL);
+	line = extract_line(stash);
+//	stash = update_stash(stash);
 	return (stash);
 }
